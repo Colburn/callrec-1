9aa64d943b661abf136acc81b2c071f4bdabc888
CameronHayden <cameron.b.hayden@gmail.com>
2017-09-11 18:01:08 -0500

    Update change_community_string.sh

--- change_community_string.sh
+++ change_community_string.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 #===============================================================================
 #
@@ -28,6 +28,8 @@ echo -n "Enter the community string you would like to use: "
 read new
 
 COUNTER=0
+comm=0
+snmp_comm=0
 f='/opt/callrec/monitoring/default'
 f2='/opt/callrec/monitoring/generate_nagios_services'
 
@@ -37,33 +39,50 @@ func() {
   sed -i "s/$old/\$_HOSTSNMPCOMMUNITY\$/g" $f/etc/nagios/zoom/commands_callrec.cfg
   sed -i 's/check_snmp -H $HOSTADDRESS$ /check_snmp -C $_HOSTSNMPCOMMUNITY$ -H $HOSTADDRESS$ /g' $f/etc/nagios/zoom/commands_callrec.cfg $f/etc/nagios/zoom/commands.cfg 
   sed -i 's/check_snmp!-o /check_snmp! -C $_HOSTSNMPCOMMUNITY$ -o /g' $f/etc/nagios/zoom/services/redlines.cfg  
-  grep -rl $old $f/etc/snmp/* | xargs sed -i "s/$old/$new/g"
+  for i in `ls $f/etc/snmp/snmp*`; do
+    sed -i "s/$old/$new/g" $i
+  done 
+#  grep -rl $old $f/etc/snmp/* | xargs sed -i "s/$old/$new/g"
   
 
   snmp_comm=`grep -rl _SNMPCOMMUNITY $f2`
   
   if (( "${#snmp_comm}" == "0" ))
   then
-    sed -i "s/use        callrec-host/use        callrec-host\n    _SNMPCOMMUNITY $new/g" $snmp_comm
+    sed -i "s/use        callrec-host/use        callrec-host\n    _SNMPCOMMUNITY $new/g" $f2
   else 
-    sed -i "s/_SNMPCOMMUNITY $old/_SNMPCOMMUNITY $new/g" $snmp_comm
+    sed -i "s/_SNMPCOMMUNITY $old/_SNMPCOMMUNITY $new/g" $f2
   fi
 
 }
+func
    
   comm=`grep -rl $old /etc/nagios/*`
+  comm2=`grep -rl $old /etc/snmp*`
   
-  if (( "${#comm}" != "0" )); then
+  if (( "${#comm}" != "0" || "${#comm2}" != "0" )); then
+
+    for i in `ls /etc/nagios/zoom/host*`; do
  
-    f=''
-    f2=/etc/nagios/zoom/host*/host.cfg
-    func 
-   fi
+      f=''
+      f2='/etc/nagios/zoom/host*/host.cfg'
+      func 
+    done
 
+  fi
 
-if (( ${#f} == 0 ))
-then
-  /etc/init.d/snmpd restart
-  /etc/init.d/nagios restart
-fi
+restart_services() {
+  echo -n "Would you like to restart the Nagios and SNMP services? [y/N]: "
+  read rstrt
 
+  if [ "$rstrt" == "Y" ] || [ "$rstrt" == "y" ]; then
+     /etc/init.d/snmpd restart
+     /etc/init.d/nagios restart
+  elif [ "$rstrt" == "N" ] || [ "$rstrt" == "n" ]; then
+    exit 0 
+  else
+    echo -n "Invalid Option: You must choose either 'Y' or 'N'"
+    restart_services
+  fi
+}
+restart_services
